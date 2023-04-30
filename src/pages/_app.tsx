c0638d30aa53a80aadcc0787b5d04b4f8e851ae1
Philix27 <philixbob@gmail.com>
2023-04-30 10:52:58 +0100

    more cleanup

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import { type AppType } from "next/app";
 import "../styles/index.scss";
+// import "../src/styles/index.scss";
 import Layout from "../comps/global/Layout";
 
 const MyApp: AppType = ({ Component, pageProps: { ...pageProps } }) => {
--- src/pages/home.tsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import HomeView from "../views/home/home.views";
-
-export default function HomePage() {
-  return <HomeView />;
-}
